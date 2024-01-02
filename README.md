4a388a3896fc71b14ba120a49d6a759d5350f847
Annie Phan <annie.phan@datastax.com>
2024-01-02 08:55:11 -0600

    Add files to public repo

---
+++ README.md
@@ -0,0 +1,26 @@
+# Fashion Buddy Demo - Astra Vector Search and Vertex AI
+
+### Intro
+Have you ever scrolled through photo sharing websites such as Instagram or Pinterest and had your eyes set on a specific outfit but did not know where to get the items? The Shopping Buddy lets you upload a photo of any outfit and returns the most similar pieces of apparel to recreate the outfit. 
+
+### Overview
+This repo contains a Streamlit app which uses Vertex AI, Gemini, RAG, and Astra DB to perform vector similarity search and multi-modal Retrieval Augmented Generation (RAG). The same code is available in a Colab here: [Link to Colab](https://colab.research.google.com/drive/1XQm_gBCZ-xRcUj4oaxcgNDqQtZLg2NpD#scrollTo=A2hnFk7YlWAN)
+
+### Before you get started
+Make sure you have the following:
+* A free Astra DB account. You can sign up with a free account [here](https://astra.datastax.com)
+* A Vertex AI account. Sign up [here](https://cloud.google.com/vertex-ai?hl=en)
+
+### Credentials
+* Download your service account key as a json file from Google Cloud console. Record the path to this json file. [See instructions here](https://cloud.google.com/iam/docs/keys-create-delete)
+* Create a `.env` file in the same directory as `fashion_buddy.py`
+* Copy/paste the following into your `.env` file and replace with your environment variables.
+```
+GCP_PROJECT_ID = "<YOUR_GCP_PROJECT_ID>"
+ASTRA_DB_TOKEN= "AstraCS:..."
+ASTRA_API_ENDPOINT= "https://<DATABASE_ID>-<REGION>.apps.astra.datastax.com"
+GOOGLE_APPLICATION_CREDENTIALS_PATH= <./PATH/TO/YOUR/GOOGLE_SERVICE_ACCOUNT_KEY.json>
+```
+
+### Run the Application
+In your terminal, run `streamlit run fashion_buddy.py` from the same directory as `fashion_buddy.py`
---
+++ streamlit_app/fashion_buddy.py
@@ -0,0 +1,187 @@
+import streamlit as st
+from PIL import Image as PILImage
+import io, os, vertexai
+from vertexai.preview.vision_models import MultiModalEmbeddingModel, Image
+from astrapy.db import AstraDB, AstraDBCollection
+from dotenv import find_dotenv, load_dotenv
+
+# Get environment variables
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+# Load Google Service Account
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_PATH")
+
+# Use Gemini Pro Vision as our LLM + Embedding Model
+vertexai.init(project=os.getenv("GCP_PROJECT_ID"))
+model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding@001")
+
+# Connect to AstraDB    
+astra_db = AstraDB(
+    token = os.getenv("ASTRA_DB_TOKEN"),
+    api_endpoint = os.getenv("ASTRA_API_ENDPOINT"))
+
+# Connect to Collection
+collection = AstraDBCollection(
+    collection_name="shopping_buddy_demo", astra_db=astra_db
+)
+
+# Create category mapping from UI selections to backend DB
+category_mapping = {
+    "Tops": "TOPS",
+    "Dresses/Jumpsuits": "DRESSES_JUMPSUITS",
+    "Outerwear": "OUTERWEAR",
+    "Bottoms": "BOTTOMS",
+    "Accessories": "ACCESSORIES",
+    "Activewear":"ACTIVEWEAR",
+    "Shoes": "SHOES",
+}
+
+# Create embedding of user uploaded image using Gemini Pro Vision
+def get_img_embeddings(img_reference, text=""):
+    img = Image.load_from_file(img_reference)
+    embeddings = model.get_embeddings(
+      image=img,
+      contextual_text=text
+  )
+    return embeddings.image_embedding
+
+# Save the user's uploaded image into the Assests folder
+def saveImage(image, file_name):
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    # Define the directory and file path
+    directory = "./Assets"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file_path = os.path.join(directory, file_name)
+
+    # Write the uploaded file to the Assets Folder
+    with open(file_path, 'wb') as f:
+        f.write(img_byte_arr)
+
+    reference_url = os.path.join(directory, file_name)
+    return reference_url
+
+# Print the recommendations to the UI
+def show_recommendations(documents, category=None):
+  if category:
+    st.write("Category:", category)
+  for doc in documents:
+    st.write(doc["product_name"], "$" + str(doc["price"]), ", Gender:", doc["gender"])
+    st.write(doc["details"])
+    st.write(doc["link"])
+    st.write(("Similarity score: ", doc["$similarity"]))
+    st.image(doc["product_images"], width=300)
+
+#Use vector search to find the similar produts in the collection
+def find_similar_products(uploaded_image_url, gender, clothing_types):    
+    if clothing_types:
+        for category in clothing_types:
+            # Run multiple ANN searches with filtering to pull top matches
+            # for each category requested
+            search_prompt = """
+                I am trying to find pieces of apparel that are similar to what is in this
+                picture. Ignore the model and only focus on finding the most similar clothing.
+
+                I only care about apparel that falls within the category contained
+                within triple backticks:
+
+            ```{category}```
+            """.format(category=category)
+            reference_embeddings = get_img_embeddings(
+                uploaded_image_url,
+                text=search_prompt
+            )
+
+            documents = collection.vector_find(
+                reference_embeddings,
+                limit=3,
+                filter={"category": category, "gender": gender.lower()},
+                include_similarity=True
+            )
+            show_recommendations(documents, category=category)
+    else:
+        # Run single ANN search across entire DB by gender
+        search_prompt = """
+        I am trying to find pieces of apparel that are similar to what is in this
+        picture. Pretend as if there is no model in the image, only clothing.
+        """
+        reference_embeddings = get_img_embeddings(
+            uploaded_image_url, 
+            text=search_prompt
+        )
+
+        documents = collection.vector_find(
+            reference_embeddings,
+            limit=3,
+            filter={"gender": gender.lower()},
+        )
+
+        show_recommendations(documents)
+
+
+def main():
+    # Display the Title + Description
+    st.title("Fashion Buddy🎄🤖")
+    st.write("*Fashion Buddy is where Vector Search 🤝 Fashion.*") 
+    st.write("With the holiday season coming up, say goodbye to wardrobe dilemmas and hello to effortless style. It's designed to help YOU find the best outfits and clothing items! All you need to do is upload any outfit or piece of clothing, and we'll find outfits that are similar. Give it a try!")
+
+    # Add DataStax Logo:
+    with st.sidebar:
+         st.image('./Assets/datastax-logo.svg')
+         st.text('')
+
+    # Add Clothing Filters header
+    with st.sidebar:
+        st.header("Clothing Filters")
+
+        # Gender selection
+        gender = st.radio ("Select a Gender:",('Men', 'Women'))
+
+        # Clothing type selection
+        st.write("👡 Select Types of Clothing 👕:")
+        categories = category_mapping.keys()
+        clothing_types = [cat for cat in categories if st.checkbox(cat)]
+        converted_clothing_types = [category_mapping.get(cat, cat) for cat in clothing_types]
+
+    # Button for user to download an example image
+    with st.sidebar:
+        st.write("⬇️ Download an example image:⬇️")
+        st.download_button(
+            label='Download Image',
+            data=open('./Assets/example-outfit.jpeg', 'rb').read(),
+            file_name='example-outfit.jpeg',
+            mime='image/jpeg'
+        )
+
+    # Drag and drop file uploader
+    uploaded_file = st.file_uploader("Upload any outfit or piece of clothing...", type=["jpg", "png", "jpeg"])
+
+    # Display uploaded file
+    if uploaded_file is not None:
+        file_name = uploaded_file.name
+        image = PILImage.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+        # Button to trigger embedding and search
+        if st.button('Recommend Products'):
+            with st.spinner('Finding similar products...'):
+                reference_image_url = saveImage(image, file_name)
+                find_similar_products(
+                    reference_image_url, 
+                    gender, 
+                    converted_clothing_types
+                )
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
